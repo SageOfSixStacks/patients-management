5ffc48f75d6bc8edb58a8d4284d6c706d5802dc3
SageOfSixStacks <collinsshakleton@gmail.com>
2026-04-09 18:54:50 +0000

    feat: add patient response dto

---
+++ patient-service/src/main/java/com/pm/patient_service/dtos/PatientResponseDto.java
@@ -0,0 +1,15 @@
+package com.pm.patient_service.dtos;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.UUID;
+
+public record PatientResponseDto(
+    UUID id,
+    String name,
+    String email,
+    String address,
+    LocalDate dateOfBirth,
+    Instant registeredDate) {
+
+}
